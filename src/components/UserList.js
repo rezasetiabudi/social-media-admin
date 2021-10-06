ee1c32fb91396cb58d3a3bdf332cb84d7430e018
rezasetiabudi <shinichisetiabudi@gmail.com>
2021-10-07 00:08:34 +0700

    adding feature rename, can reuseable to other data edit, but i still struggle with redux flow and syntax

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import { getUsers, deleteUser } from "../store/actions/usersActions";
 
 import { Button, Box, Flex } from "@chakra-ui/react";
 
 import { Heading } from "@chakra-ui/layout";
+
+import FormComponent from "./FormComponent";
 
 const mapStateToProps = (state) => ({
   users: state.users,
@@ -15,18 +17,27 @@ const mapStateToProps = (state) => ({
 });
 
 class UserList extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditShow: true,
+    };
+  }
+
   componentDidMount() {
     this.props.getUsers();
   }
 
   render() {
     const { users } = this.props.users;
-
     console.log(this.props);
 
     return (
       <div>
         <BrowserRouter>
+        <Route path="/formRename/:id" exact component={FormComponent} />
+        {console.log("userlist page(render)", users)}
         {users &&
           users.map((u) => (
             <React.Fragment key={u.id}>
@@ -40,9 +51,11 @@ class UserList extends Component {
                 >
                   <Heading>{u.name}</Heading>
                   <Box>
-                    <Button mx={1}>
-                      rename account
-                    </Button>
+                    <Link to={`/formRename/${u.id}`}>
+                      <Button mx={1}>
+                        rename account
+                      </Button>
+                    </Link>
                     <Button mx={1} onClick={() => this.props.deleteUser(u.id)}>
                       remove account
                     </Button>
--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -1,4 +1,4 @@
-import { DELETE_USER, GET_USERS, USERS_ERROR } from "../types";
+import { DELETE_USER, GET_USERS, PUT_USERS, USERS_ERROR } from "../types";
 import axios from "axios";
 
 import API_URL from "../../constants";
@@ -9,6 +9,22 @@ export const getUsers = () => async (dispatch) => {
 
     dispatch({
       type: GET_USERS,
+      payload: res.data
+    });
+  } catch (e) {
+    dispatch({
+      type: USERS_ERROR,
+      payload: console.log(e)
+    });
+  }
+};
+
+export const putUsers = (data, id) => async (dispatch) => {
+  try {
+    const res = await axios.put(API_URL+'/users/'+id, data);
+    console.log("actions, api resp", res)
+    dispatch({
+      type: PUT_USERS,
       payload: res.data
     });
   } catch (e) {
